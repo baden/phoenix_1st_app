8a949e6c371d8a5ecebf27aa3c793e8929f3d4d7
Denys Batrak <baden.i.ua@gmail.com>
2016-08-11 01:20:15 +0300

    Add somy dynamics. Yesgit add .! He is alivegit add .git add .

--- phoenix_1st/web/static/js/components/hello.js
+++ phoenix_1st/web/static/js/components/hello.js
@@ -5,24 +5,51 @@ import List from './list';
 
 console.log("Wtf styles", styles);
 
-var data = [
+var data1 = [
   {id: 1, author: "Pete Hunt", text: "This is one comment"},
   {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
 ];
 
+var data2 = [
+  {id: 1, author: "Денис Батрак", text: "Это взлом, всем стоять на месте!"},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
 
-class Hello extends React.Component {
-  render() {
+// var initState
+
+// class Hello extends React.Component {
+var Hello = React.createClass({
+  // componentDidMount() {
+  //   // const { data } = data;
+  //   this.props.data = data;
+  //   console.log("Hello.mount");
+  // }
+  getInitialState: () => {
+    return {
+      data: data1
+    };
+  },
+
+  componentDidMount: function() {
+    var that = this;
+    setTimeout(() => {
+      console.log("Boo");
+      that.setState({data: data2});
+    }, 3000);
+  },
+
+  render: function() {
+    console.log("Hello.props = ", this);
     return (
       <div>
         <h1 className={styles.title}>
           Hello, {this.props.name}!!!
         </h1>
-        <List data={data} />
+        <List data={this.state.data} />
       </div>
     );
   }
-}
+});
 //
 
 export default Hello
